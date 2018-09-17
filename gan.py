962b4541acdedaf6fbc1c87b0ae16cbbdc1622ad
innnk <innnk1989@gmail.com>
2018-09-17 20:58:09 +0800

    update code

--- gan.py
+++ gan.py
@@ -78,7 +78,7 @@ def stats(d):
 def decorate_with_diffs(data, exponent):
     mean = torch.mean(data.data, 1, keepdim=True)
     mean_broadcast = torch.mul(torch.ones(data.size()), mean.tolist()[0][0])
-    diffs = torch.pow(data - Variable(mean_broadcast), exponent)
+    diffs = torch.pow(data - mean_broadcast, exponent)
     return torch.cat([data, diffs], 1)
 
 d_sampler = get_distribution_sampler(data_mean, data_stddev)
@@ -95,16 +95,16 @@ for epoch in range(num_epochs):
         D.zero_grad()
 
         #  1A: Train D on real
-        d_real_data = Variable(d_sampler(d_input_size))
+        d_real_data = d_sampler(d_input_size)
         d_real_decision = D(preprocess(d_real_data))
-        d_real_error = criterion(d_real_decision, Variable(torch.ones(1)))  # ones = true
+        d_real_error = criterion(d_real_decision, torch.ones(1))  # ones = true
         d_real_error.backward() # compute/store gradients, but don't change params
 
         #  1B: Train D on fake
-        d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+        d_gen_input = gi_sampler(minibatch_size, g_input_size)
         d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
         d_fake_decision = D(preprocess(d_fake_data.t()))
-        d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(1)))  # zeros = fake
+        d_fake_error = criterion(d_fake_decision, torch.zeros(1))  # zeros = fake
         d_fake_error.backward()
         d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
 
@@ -112,10 +112,10 @@ for epoch in range(num_epochs):
         # 2. Train G on D's response (but DO NOT train D on these labels)
         G.zero_grad()
 
-        gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+        gen_input = gi_sampler(minibatch_size, g_input_size)
         g_fake_data = G(gen_input)
         dg_fake_decision = D(preprocess(g_fake_data.t()))
-        g_error = criterion(dg_fake_decision, Variable(torch.ones(1)))  # we want to fool, so pretend it's all genuine
+        g_error = criterion(dg_fake_decision, torch.ones(1))  # we want to fool, so pretend it's all genuine
 
         g_error.backward()
         g_optimizer.step()  # Only optimizes G's parameters
