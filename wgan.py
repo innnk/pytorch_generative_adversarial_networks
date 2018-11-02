5d861e84b392ee9c9bdae0850ef7a99704c47a2d
innnk <innnk1989@gmail.com>
2018-11-02 23:16:41 +0800

    update readme

--- wgan.py
+++ wgan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Wasserstein Generative Adversarial Networks (GAN) example in PyTorch.
+# Wasserstein Generative Adversarial Networks (GAN) example in PyTorch
 
 import numpy as np
 import torch
